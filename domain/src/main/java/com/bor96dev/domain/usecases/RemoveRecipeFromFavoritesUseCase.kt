708cafa142d1879f14f6da686494bc83d25fef38
jakobakoba <borisgabyshev96dev@gmail.com>
2025-08-06 00:04:40 +0900

    create more usecases

---
+++ domain/src/main/java/com/bor96dev/domain/usecases/RemoveRecipeFromFavoritesUseCase.kt
@@ -0,0 +1,13 @@
+package com.bor96dev.domain.usecases
+
+import com.bor96dev.domain.Recipe
+import com.bor96dev.domain.RecipeRepository
+import javax.inject.Inject
+
+class RemoveRecipeFromFavoritesUseCase @Inject constructor(
+    private val repository: RecipeRepository
+) {
+    suspend operator fun invoke(recipe: Recipe){
+        repository.removeRecipeFromFavorites(recipe)
+    }
+}
---
+++ presentation/src/main/java/com/bor96dev/presentation/favorites/FavoriteRecipesViewModel.kt
@@ -0,0 +1,6 @@
+package com.bor96dev.presentation.favorites
+
+import androidx.lifecycle.ViewModel
+
+class FavoriteRecipesViewModel: ViewModel() {
+}
