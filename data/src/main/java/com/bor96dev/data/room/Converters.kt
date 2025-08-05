f7f01bbf467da86a81683c293eed877379b6b463
jakobakoba <borisgabyshev96dev@gmail.com>
2025-08-05 22:11:50 +0900

    create typeconverters for room

--- data/src/main/java/com/bor96dev/data/room/Converters.kt
+++ data/src/main/java/com/bor96dev/data/room/Converters.kt
@@ -1,4 +1,27 @@
 package com.bor96dev.data.room
 
+import androidx.room.TypeConverter
+import com.bor96dev.data.remote.dto.IngredientDto
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 class Converters {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromIngredientDtoList(ingredients: List<IngredientDto>?): String ?{
+        if (ingredients == null){
+            return null
+        }
+        return gson.toJson(ingredients)
+    }
+
+    @TypeConverter
+    fun toIngredientDtoList(ingredientsString: String?): List<IngredientDto>? {
+        if (ingredientsString == null){
+            return null
+        }
+        val listType = object: TypeToken<List<IngredientDto>>() {}.type
+        return gson.fromJson(ingredientsString, listType)
+    }
 }
--- data/src/main/java/com/bor96dev/data/room/MealMagicianDatabase.kt
+++ data/src/main/java/com/bor96dev/data/room/MealMagicianDatabase.kt
@@ -2,8 +2,10 @@ package com.bor96dev.data.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [RecipeEntity::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class MealMagicianDatabase: RoomDatabase() {
     abstract fun recipeDao(): RecipeDao
 }
