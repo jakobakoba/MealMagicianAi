d40e123779befa6954fe1ea17e9d4b93cc07f279
jakobakoba <borisgabyshev96dev@gmail.com>
2025-08-12 17:37:43 +0900

    fix image size for empty recipes

--- presentation/src/main/java/com/bor96dev/presentation/myfridge/MyFridgeScreen.kt
+++ presentation/src/main/java/com/bor96dev/presentation/myfridge/MyFridgeScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -142,7 +141,6 @@ fun MyFridgeScreen(
                     Image(
                         painter = painterResource(R.drawable.no_recipes),
                         contentDescription = "no recipes",
-                        modifier = Modifier.size(150.dp)
                     )
                 }
 
